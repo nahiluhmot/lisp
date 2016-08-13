ee968d183bd1e7211ae806baea16603118b007c8
Tom Hulihan <hulihan.tom159@gmail.com>
2016-08-13 13:20:59 -0400

    Add list.lisp

--- lib/prelude.lisp
+++ lib/prelude.lisp
@@ -1,3 +1,7 @@
+;;; This is the top level file loaded by the lisp interpreter.
+;;; It contains definitions of very basic macros (such as defmacro and defun)
+;;; and loads the rest of the standard library.
+
 ;; Define a macro with the given name, arguments, and body.
 (def defmacro (macro (name args . body)
                      `(def ,name (macro ,args ,@body))))
@@ -5,3 +9,5 @@
 ;; Define a function with the given name, arguments, and body.
 (defmacro defun (name args . body)
   `(def ,name (lambda ,args ,@body)))
+
+(load "prelude/list")
---
+++ lib/prelude/list.lisp
@@ -0,0 +1,63 @@
+;;; list.lisp: list utilities.
+
+(defun empty? (xs)
+  (== 'nil (type-of xs)))
+
+(defun foldl (f initial list)
+  (let ((go (lambda (acc xs)
+              (if xs
+                  (recur (f acc (first xs)) (rest xs))
+                acc))))
+    (go initial list)))
+
+(defun foldr (f initial list)
+  (let ((go (lambda (acc xs)
+              (if xs
+                  (recur (f (last xs) acc) (but-last xs))
+                acc))))
+    (go initial list)))
+
+(defun unfoldr (f init)
+  (let ((go (lambda (curr acc)
+              (let ((next (f curr)))
+                (if next
+                    (recur next (cons curr acc))
+                  acc)))))
+    (go init ())))
+
+(defun unfoldl (f init)
+  (let ((go (lambda (curr acc)
+              (let ((next (f curr)))
+                (if next
+                    (recur next (snoc acc curr))
+                  acc)))))
+    (go init ())))
+
+(defun reverse (xs)
+  (foldl (lambda (x y) (cons y x)) () xs))
+
+(defun map (f xs)
+  (foldl (lambda (ys x) (snoc ys (f x))) () xs))
+
+(defun select (f xs)
+  (foldl (lambda (ys x) (if (f x) (snoc ys x) ys)) () xs))
+
+(defun reject (f xs)
+  (select (lambda (x) (not (f x))) xs))
+
+(defun take-while (f xs)
+  (let ((go (lambda (ys acc)
+              (if ys
+                  (if (f (first ys))
+                      (recur (rest ys) (snoc acc (first ys)))
+                    acc)
+                acc))))
+    (go xs ())))
+
+(defun drop-while (f xs)
+  (let ((go (lambda (ys)
+              (if ys
+                  (if (f (first ys))
+                      (recur (rest ys))
+                    ys)))))
+    (go xs)))
