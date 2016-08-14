f04aa1d0ac228c63d35a5bcf58cab4b9e19b02d2
Tom Hulihan <hulihan.tom159@gmail.com>
2016-08-14 14:13:33 -0400

    Read from ARGV and the environment

--- src/Lisp/Prelude/IO.hs
+++ src/Lisp/Prelude/IO.hs
@@ -4,8 +4,11 @@ module Lisp.Prelude.IO where
 
 import Control.Monad.State.Strict
 import Data.Functor
+import Data.Sequence
 import qualified Data.Text.IO as IO
+import qualified Data.Text as T
 import System.IO (hFlush, stdout)
+import System.Environment
 
 import Lisp.Data
 import Lisp.Core
@@ -18,3 +21,17 @@ defPreludeIO = do
       String str -> liftIO (IO.putStr str >> hFlush stdout) $> Nil
       _ -> raiseTypeMismatch "string" sexp
   defun0 "gets" $ String <$> liftIO IO.getLine
+
+  rawArgv <- liftIO getArgs
+  def' "*argv*" $ list . fromList $ map (String . T.pack) rawArgv
+
+  defun1 "get-env" $ \k ->
+    case k of
+      (String key) -> maybe Nil (String . T.pack) <$> liftIO (lookupEnv (T.unpack key))
+      _ -> raiseTypeMismatch "string" k
+
+  defun2 "set-env" $ \k v ->
+    case (k, v) of
+      (String key, String val) -> liftIO (setEnv (T.unpack key) (T.unpack val)) $> Nil
+      (String _, val) -> raiseTypeMismatch "string" val
+      (key, _) -> raiseTypeMismatch "string" key
