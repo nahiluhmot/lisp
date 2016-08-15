2a841a567fd3e607fc4c165dcfdb1beeb6cc98a9
Tom Hulihan <hulihan.tom159@gmail.com>
2016-08-14 23:56:19 -0400

    Add a primes example

---
+++ example/primes.lisp
@@ -0,0 +1,21 @@
+;;; primes.lisp: example program which generates prime numbers.
+
+(defun range (start end)
+  (let ((go (lambda (curr acc)
+              (if (> curr end)
+                  acc
+                (recur (+ curr 1) (snoc acc curr))))))
+    (go start ())))
+
+(defun primes-to (n)
+  (let ((go (lambda (curr acc primes)
+              (let ((filtered
+                     (select (lambda (prime) (!= 0 (% prime curr))) acc))
+                    (next-primes (snoc primes curr)))
+                (if filtered
+                    (recur (first filtered) (rest filtered) next-primes)
+                  next-primes)))))
+    (if (>= n 2)
+        (go 2 (range 2 n) ()))))
+
+(puts (primes-to (first (read (first *argv*)))))
