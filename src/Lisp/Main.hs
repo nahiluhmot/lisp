7e42d4072f32edae6813e4d11d2e5a144fad3b2b
Tom Hulihan <hulihan.tom159@gmail.com>
2018-06-20 09:17:06 -0400

    Better error messages for unhandled errors

--- src/Lisp/Main.hs
+++ src/Lisp/Main.hs
@@ -5,6 +5,7 @@ module Lisp.Main where
 
 import Prelude hiding (getLine, putStrLn, putStr)
 
+import Data.Monoid ((<>))
 import Data.Text
 import Data.Text.IO
 
@@ -12,11 +13,14 @@ import Lisp.Data
 import Lisp.Core
 import Lisp.Prelude
 import Lisp.VirtualMachine
+import qualified Lisp.SymbolTable as ST
 
 lispMain :: IO ()
 lispMain = do
-  (result, _) <- runLispM (defPrelude >> funcallByName "load" [String "lisp"]) emptyLispState
+  (result, state) <- runLispM (defPrelude >> funcallByName "load" [String "lisp"]) emptyLispState
   case result of
-    Left err -> do
-      putStrLn $ "*** unhandled error: " `append` pack (show err)
+    Left err@(LispError sym msg) ->
+      case ST.idToSym sym (symbolTable state) of
+        Nothing -> putStrLn $ "*** unhandled error: " <> pack (show err)
+        Just typ -> putStrLn $ "*** unhandled error of type " <> typ <> ": " <> msg
     Right _ -> return ()
